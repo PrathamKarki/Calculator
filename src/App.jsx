7bb8adaf6319d4ab9344a860af7abc4d0bab5ac5
pratham karki <prathamkarki21@gmail.com>
2025-05-25 17:45:08 +0545

    layout and styling for calculator

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from "react";
 
+const Calculator = () =>{
+  const symbols = ['+', '-', '*', '/', '.', '%'];
 
-function App() {
-  const [count, setCount] = useState(0)
+  const [result, setResult] = useState('');
 
-  return (
-    <>
-      <div className='border-2'>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  const buttons = [
+    
+    ['C','(', ')', '/'],
+    ['7', '8', '9', '*'],
+    ['4', '5', '6', '-'],
+    ['1', '2', '3', '+'],
+    ['0', '.', '%', '⌦'],
+    ['=']
+  ]
+
+  const bracketsAndClear = ['C', '(', ')']
+
+  
+  return(
+      <section className="main_container border-1 w-[450px] p-5  bg-[#32161F] text-white" >
+        <div className="input_container  border-1 p-5 mb-3 bg-[#F0FFFF] mx-auto w-[394px]">
+          <div className="inputField text-4xl text-right mr-2 text-black">0</div>
+          </div>
+          <div className="flex_calculator_container  p-2 mb-3 flex flex-wrap w-[100%] gap-[5px] text-black " >
+            {buttons.map((item, id)=>{
+              return item.map((value,idx)=>{
+                return (
+                <button className={` text-2xl w-[90px] h-[50px] grow border-white text-[#eee]   }
+                 ${value === '=' ? 'bg-[#F13F3F]': value === '⌦' ? 'bg-[#1E90FF]': bracketsAndClear.includes(value) ? 'bg-[#1E90FF]' : symbols.includes(value) ? 'bg-[#FF8F1F]': 'bg-[#217C7E]' }`} key={idx}>{value}</button>
+              )
+              })
+            })}
+          </div>
+        
+      </section>
+
   )
 }
-
-export default App
+export default Calculator;
